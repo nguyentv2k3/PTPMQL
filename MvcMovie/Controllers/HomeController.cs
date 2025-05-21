0dafcd2fd9c70c296b406de73517975787f8d66e
nguyentv2k3@gmail.com <nguyentv2k3@gmail.com>
2025-05-21 08:53:15 +0700

    Chuong7_slide61

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -17,6 +17,13 @@ public class HomeController : Controller
     {
         return View();
     }
+    [HttpPost]
+    public IActionResult Index(string FullName, string Address)
+    {
+        string strOutput = "Xin chao" + FullName + "den tu" + Address;
+        ViewBag.Message = strOutput;
+        return View();
+    }
 
     public IActionResult Privacy()
     {
